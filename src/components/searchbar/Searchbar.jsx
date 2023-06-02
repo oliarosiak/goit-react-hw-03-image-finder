67fad471c7a611517f93d31a29c237854e200e17
Olia Rosiak <olia.rosiak@gmail.com>
2023-06-02 22:58:18 +0300

    add start component, update Searchbar, update App

---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import css from './Searchbar.module.css';
+
+class Searchbar extends Component{
+
+    render() {
+        
+        return (
+            <header className={css.Searchbar}>
+                <form className={css.SearchForm}>
+                    <button type="submit" className={css.SearchFormButton}>
+                        <span className={css.SearchFormButtonLabel}>Search</span>
+                    </button>
+
+                    <input
+                        className={css.SearchFormInput}
+                        type="text"
+                        autocomplete="off"
+                        autofocus
+                        placeholder="Search images and photos"
+                    />
+                </form>
+            </header>
+        )
+    }
+};
+
+export default Searchbar;
