3e09ade68daa717e3d559d8b66a0d857f3679ea6
Olia Rosiak <olia.rosiak@gmail.com>
2023-06-06 12:59:17 +0300

    update ImageGalleryItem component

--- src/components/imageGalleryItem/ImageGalleryItem.jsx
+++ src/components/imageGalleryItem/ImageGalleryItem.jsx
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
 import css from './ImageGalleryItem.module.css';
 
-class ImageGalleryItem extends Component{
-
-    render() {        
-        const { arrayOfImages, onClick } = this.props; 
-        // console.log('masyv', arrayOfImages);
-        return (
-            <>
-                {arrayOfImages.map(({ id, webformatURL, largeImageURL, tags }) => {
-                    return <li key={id} className={css.ImageGalleryItem}>
-                        <img src={webformatURL} alt={tags}
-                            className={css.ImageGalleryItemImage} onClick={() => onClick(largeImageURL, tags)} />
-                    </li>
-                })}
-            </>
-        );
-    }
-}
+const ImageGalleryItem = ({ arrayOfImages, onImageClick }) => (
+    <>
+        {arrayOfImages.map(({ id, webformatURL, largeImageURL, tags }) => {
+            return <li key={id} className={css.ImageGalleryItem}>
+                <img src={webformatURL} alt={tags}
+                className={css.ImageGalleryItemImage} onClick={()=>onImageClick(largeImageURL,tags)} />
+            </li>
+        })}    
+    </>
+)
 
 export default ImageGalleryItem;
