453aa153b3f60f5c6de3c110f421635ad2dbb4d1
Olia Rosiak <olia.rosiak@gmail.com>
2023-06-06 12:59:43 +0300

    update Button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,28 +1,12 @@
-import React, { Component } from "react";
 import css from './Button.module.css';
 
-class Button extends Component{
-    state = {
-        page: 2,
-    }
-
-    handleLoadMoreBtn = () => {      
-        this.setState(prevState => {
-            return { page: prevState.page + 1 }
-        });
-        this.props.onClick(this.state.page);
-    }
-
-    render() { 
-        return (
-            <button
-                onClick={this.handleLoadMoreBtn}
-                type="button"
-                className={css.Button} >
-                Load more
-            </button>
-        )
-    }
-}
+const Button = ({ onButtonClick }) => (
+    <button
+        onClick={() => onButtonClick()}
+        type="button"
+        className={css.Button} >
+        Load more
+    </button>
+);
 
 export default Button;
